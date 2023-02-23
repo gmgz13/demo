3c6504c0ab31a2ccb9a92368c856319ddfb04d1a
gmgz13 <1302059006@qq.com>
2023-02-23 17:42:03 +0800

    调整

--- src/utils/TrackResource.ts
+++ src/utils/TrackResource.ts
@@ -5,13 +5,12 @@ export default class ResourceTracker {
     constructor() {
         this.resources = new Set();
     }
+    //层层遍历释放内存
     track(resource:any) {
         if (!resource) {
             return resource;
         }
 
-        // handle children and when material is an array of materials or
-        // uniform is array of textures
         if (Array.isArray(resource)) {
             resource.forEach(resource => this.track(resource));
             return resource;
@@ -25,13 +24,11 @@ export default class ResourceTracker {
             this.track(resource.material);
             this.track(resource.children);
         } else if (resource instanceof Material) {
-            // We have to check if there are any textures on the material
             for (const value of Object.values(resource)) {
                 if (value instanceof Texture) {
                     this.track(value);
                 }
             }
-            // We also have to check if any uniforms reference textures or arrays of textures
             if ((resource as any).uniforms) {
                 for (const value of Object.values((resource as any).uniforms)) {
                     if (value) {
